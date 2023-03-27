801dc8abb7b0fcc95bebf5ee93413bb910552ee8
fulgurance <zohran.londais@gmail.com>
2023-03-27 15:34:10 +0100

    Patched configure error

--- Findutils/4.8.0/4.8.0.cr
+++ Findutils/4.8.0/4.8.0.cr
@@ -7,7 +7,7 @@ class Target < ISM::Software
             configureSource([   "--prefix=/usr",
                                 "--localstatedir=/var/lib/locate",
                                 "--host=#{Ism.settings.target}",
-                                "--build=$(build-aux/config.guess)"],
+                                "--build=#{Ism.settings.target}"],
                                 buildDirectoryPath)
         else
             configureSource([   "--prefix=/usr",
