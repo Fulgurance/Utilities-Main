6390b20e7c46a0e13472f096e19c1fd336962ce0
fulgurance <zohran.londais@gmail.com>
2023-03-27 15:28:16 +0100

    Patched configure error

--- File/5.40.0/5.40.0.cr
+++ File/5.40.0/5.40.0.cr
@@ -22,7 +22,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource([   "--prefix=/usr",
                                 "--host=#{Ism.settings.target}",
-                                "--build=$(./config.guess)"],
+                                "--build=#{Ism.settings.target}"],
                                 buildDirectoryPath)
         else
             configureSource([   "--prefix=/usr"],
