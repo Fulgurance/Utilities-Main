8f6e2903aac024b01c77e17db8e30e989d171293
fulgurance <zohran.londais@gmail.com>
2023-03-27 13:40:46 +0100

    Added openrc support

--- Sysklogd/1.5.1/1.5.1.cr
+++ Sysklogd/1.5.1/1.5.1.cr
@@ -2,17 +2,20 @@ class Target < ISM::Software
 
     def prepare
         super
+
         fileDeleteLine("#{buildDirectoryPath(false)}ksym_mod.c",192)
         fileReplaceText("#{buildDirectoryPath(false)}syslogd.c","union wait","int")
     end
     
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
     
     def prepareInstallation
         super
+
         makeSource([Ism.settings.makeOptions,"BINDIR=/sbin","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc")
 
@@ -26,6 +29,18 @@ class Target < ISM::Software
         *.emerg *
         CODE
         fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/syslog.conf",syslogData)
+
+        if option("Openrc")
+            sysklogdData = <<-CODE
+            # Config file for /etc/init.d/sysklogd
+            SYSLOGD="-m 0 -s -s -r 10M:10"
+            CODE
+            fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/conf.d/sysklogd",sysklogdData)
+
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+            moveFile("#{workDirectoryPath(false)}sysklogd.rc10","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/sysklogd")
+            runChmodCommand(["+x","sysklogd"],"#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d")
+        end
     end
 
 end
