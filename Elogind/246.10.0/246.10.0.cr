c6e52f42366296bd883dcc3ae8169925400449c6
Fulgurance <zohran.londais@gmail.com>
2023-04-13 11:06:42 +0100

    Improved implementation

--- Elogind/246.10.0/246.10.0.cr
+++ Elogind/246.10.0/246.10.0.cr
@@ -30,7 +30,7 @@ class Target < ISM::Software
     def build
         super
 
-        runNinjaCommand([] of String,buildDirectoryPath)
+        runNinjaCommand(path: buildDirectoryPath)
     end
 
     def prepareInstallation
