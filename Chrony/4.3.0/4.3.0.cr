142fe391221489b6b4c10e0a56a6c77a2d3c665f
fulgurance <zohran.londais@gmail.com>
2023-06-01 23:27:37 +0100

    Added chrony config files

--- Chrony/4.3.0/4.3.0.cr
+++ Chrony/4.3.0/4.3.0.cr
@@ -17,6 +17,21 @@ class Target < ISM::Software
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
 
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/conf.d")
+
+        chronydData = <<-CODE
+        CFGFILE="/etc/chrony.conf"
+        ARGS=" -u ntp -F 2"
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/conf.d/chronyd",chronydData)
+
+        chronyconfData = <<-CODE
+        pool pool.ntp.org iburst auto_offline
+        makestep 1.0 3
+        rtcsync
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/chrony.conf",chronyconfData)
+
         if option("Openrc")
             prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Chrony-Init.d","chronyd")
         end
