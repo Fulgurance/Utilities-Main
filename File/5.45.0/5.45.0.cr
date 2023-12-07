8c01e7aa68cc64b70c289a9027b8782ffa629a26
Fulgurance <zohran.londais@gmail.com>
2023-12-07 15:28:50 +0000

    Added 32 bits support

--- File/5.45.0/5.45.0.cr
+++ File/5.45.0/5.45.0.cr
@@ -13,6 +13,18 @@ class Target < ISM::Software
             makeSource(path: buildDirectoryPath)
             @buildDirectory = false
         else
+            if option("32Bits") || option("x32Bits")
+                @buildDirectory = true
+            end
+
+            if option("32Bits")
+                @buildDirectoryNames["32Bits"] = "mainBuild-32"
+            end
+
+            if option("x32Bits")
+                @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
+            end
+
             super
         end
     end
@@ -27,7 +39,23 @@ class Target < ISM::Software
                                 buildDirectoryPath)
         else
             configureSource([   "--prefix=/usr"],
-                                buildDirectoryPath)
+                                path: buildDirectoryPath(entry: "mainBuild"))
+
+            if option("32Bits")
+                configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnu",
+                                    "--prefix=/usr",
+                                    "--libdir=/usr/lib32"],
+                                    path: buildDirectoryPath(entry: "32Bits"),
+                                    environment: {"CC" =>"gcc -m32"})
+            end
+
+            if option("x32Bits")
+                configureSource([   "--host=#{Ism.settings.target}x32",
+                                    "--prefix=/usr",
+                                    "--libdir=/usr/libx32"],
+                                    path: buildDirectoryPath(entry: "x32Bits"),
+                                    environment: {"CC" =>"gcc -mx32"})
+            end
         end
     end
 
@@ -37,17 +65,51 @@ class Target < ISM::Software
         if option("Pass1")
             makeSource(["FILE_COMPILE=#{buildDirectoryPath}build/src/file"],buildDirectoryPath)
         else
-            makeSource(path: buildDirectoryPath)
+            makeSource(path: buildDirectoryPath(entry: "mainBuild"))
+
+            if option("32Bits")
+                makeSource(path: buildDirectoryPath(entry: "32Bits"))
+            end
+
+            if option("x32Bits")
+                makeSource(path: buildDirectoryPath(entry: "x32Bits"))
+            end
         end
     end
 
     def prepareInstallation
         super
 
-        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource( ["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}",
+                    "install"],
+                    path: buildDirectoryPath(entry: "mainBuild"))
 
         if option("Pass1")
             deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib/libmagic.la")
+        else
+            if option("32Bits")
+                makeDirectory("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits")
+                makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+                makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits",
+                            "install"],
+                            path: buildDirectoryPath(entry: "32Bits"))
+
+                copyDirectory(  "#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32",
+                                "#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib32")
+            end
+
+            if option("x32Bits")
+                makeDirectory("#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits")
+                makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+                makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits",
+                            "install"],
+                            path: buildDirectoryPath(entry: "x32Bits"))
+
+                copyDirectory(  "#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits/usr/libx32",
+                                "#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/libx32")
+            end
         end
     end
 
