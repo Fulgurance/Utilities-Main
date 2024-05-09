f8e406e61dee54e8807e1e02b5c41cb8bee0c852
Fulgurance <zohran.londais@gmail.com>
2024-05-09 09:55:10 +0100

    Patched wrong instruction

--- Cups/2.4.6/2.4.6.cr
+++ Cups/2.4.6/2.4.6.cr
@@ -23,7 +23,7 @@ class Target < ISM::Software
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
 
-        deleteFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}tmp/cupsinit")
+        deleteDirectoryRecursively("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}tmp")
 
         clientConfData = <<-CODE
         ServerName /run/cups/cups.sock
