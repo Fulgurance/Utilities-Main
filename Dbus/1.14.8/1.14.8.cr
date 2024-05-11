71e98d2d806d9f606e8e27657c0da288d2261221
Fulgurance <zohran.londais@gmail.com>
2024-05-11 22:21:12 +0100

    Patched wrong command

--- Dbus/1.14.8/1.14.8.cr
+++ Dbus/1.14.8/1.14.8.cr
@@ -36,7 +36,7 @@ class Target < ISM::Software
 
         makeLink("/var/lib/dbus/machine-id","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/machine-id",:symbolicLink)
 
-        deleteDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}no")
+        deleteDirectoryRecursively("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}no")
     end
 
     def install
