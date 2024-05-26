82e43ed228fadaec31ba71b4a05ada15c1bde839
Fulgurance <zohran.londais@gmail.com>
2024-05-26 10:01:52 +0100

    Update file structures and removed old versions

--- Man-Pages/5.13.0/5.13.0.cr
+++
@@ -1,9 +0,0 @@
-class Target < ISM::Software
-    
-    def prepareInstallation
-        super
-
-        makeSource(["prefix=/usr","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-    end
-
-end
