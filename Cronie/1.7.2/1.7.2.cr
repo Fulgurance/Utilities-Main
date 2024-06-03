7d2280a3c1850b342df77f8ac0bf4c2f95562fe1
Fulgurance <zohran.londais@gmail.com>
2024-06-03 22:57:44 +0100

    Added proper support for linux-pam

--- Cronie/1.7.2/1.7.2.cr
+++ Cronie/1.7.2/1.7.2.cr
@@ -21,6 +21,35 @@ class Target < ISM::Software
 
         makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
 
+        if option("Linux-Pam")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/pam.d")
+
+            croniePamData = <<-CODE
+            auth       include    system-auth
+            account    required   pam_access.so
+            account    include    system-auth
+            session    required   pam_loginuid.so
+            session    include    system-auth
+            CODE
+            fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/pam.d/cronie",croniePamData)
+        end
+
+        crontabData = <<-CODE
+        SHELL=/bin/bash
+        PATH=/sbin:/bin:/usr/sbin:/usr/bin
+        MAILTO=root
+        HOME=/
+
+        # .---------------- minute (0 - 59)
+        # |  .------------- hour (0 - 23)
+        # |  |  .---------- day of month (1 - 31)
+        # |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
+        # |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+        # |  |  |  |  |
+        # *  *  *  *  * user-name  command to be executed
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/crontab",crontabData)
+
         if option("Openrc")
             prepareOpenrcServiceInstallation("#{workDirectoryPath(false)}/Cronie-Init.d","cronie")
         end
