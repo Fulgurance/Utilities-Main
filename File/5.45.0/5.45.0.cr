20999f5dd68baec6f7a50c1d116a37045b860280
Fulgurance <zohran.londais@gmail.com>
2024-07-05 19:55:15 +0100

    Removed multilib

--- File/5.45.0/5.45.0.cr
+++ File/5.45.0/5.45.0.cr
@@ -14,20 +14,6 @@ class Target < ISM::Software
             makeSource(path: buildDirectoryPath)
 
             @buildDirectory = false
-        else
-            if option("32Bits") || option("x32Bits")
-                @buildDirectory = true
-            end
-
-            if option("32Bits")
-                @buildDirectoryNames["32Bits"] = "mainBuild-32"
-            end
-
-            if option("x32Bits")
-                @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
-            end
-
-            super
         end
     end
 
@@ -41,23 +27,7 @@ class Target < ISM::Software
                             path:       buildDirectoryPath)
         else
             configureSource(arguments:  "--prefix=/usr",
-                            path:       buildDirectoryPath(entry: "MainBuild"))
-
-            if option("32Bits")
-                configureSource(arguments:      "--host=i686-#{Ism.settings.systemTargetName}-linux-gnu \
-                                                --prefix=/usr                                           \
-                                                --libdir=/usr/lib32",
-                                path:           buildDirectoryPath(entry: "32Bits"),
-                                environment:    {"CC" =>"gcc -m32"})
-            end
-
-            if option("x32Bits")
-                configureSource(arguments:      "--host=#{Ism.settings.systemTarget}x32 \
-                                                --prefix=/usr                           \
-                                                --libdir=/usr/libx32",
-                                path:           buildDirectoryPath(entry: "x32Bits"),
-                                environment:    {"CC" =>"gcc -mx32"})
-            end
+                            path:       buildDirectoryPath)
         end
     end
 
@@ -65,18 +35,10 @@ class Target < ISM::Software
         super
 
         if option("Pass1")
-            makeSource( arguments:  "FILE_COMPILE=#{buildDirectoryPath(entry: "MainBuild")}/src/file",
+            makeSource( arguments:  "FILE_COMPILE=#{buildDirectoryPath}/src/file",
                         path:       buildDirectoryPath)
         else
-            makeSource(path: buildDirectoryPath(entry: "MainBuild"))
-
-            if option("32Bits")
-                makeSource(path: buildDirectoryPath(entry: "32Bits"))
-            end
-
-            if option("x32Bits")
-                makeSource(path: buildDirectoryPath(entry: "x32Bits"))
-            end
+            makeSource(path: buildDirectoryPath)
         end
     end
 
@@ -84,32 +46,10 @@ class Target < ISM::Software
         super
 
         makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath} install",
-                    path:       buildDirectoryPath(entry: "MainBuild"))
+                    path:       buildDirectoryPath)
 
         if option("Pass1")
             deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/libmagic.la")
-        else
-            if option("32Bits")
-                makeDirectory("#{buildDirectoryPath(entry: "32Bits")}/32Bits")
-                makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-                makeSource( arguments:  "DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits install",
-                            path:       buildDirectoryPath(entry: "32Bits"))
-
-                copyDirectory(  "#{buildDirectoryPath(entry: "32Bits")}/32Bits/usr/lib32",
-                                "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib32")
-            end
-
-            if option("x32Bits")
-                makeDirectory("#{buildDirectoryPath(entry: "x32Bits")}/x32Bits")
-                makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-                makeSource( arguments:  "DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits install",
-                            path:       buildDirectoryPath(entry: "x32Bits"))
-
-                copyDirectory(  "#{buildDirectoryPath(entry: "x32Bits")}/x32Bits/usr/libx32",
-                                "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/libx32")
-            end
         end
     end
 
--- Util-Linux/2.39.1/2.39.1.cr
+++ Util-Linux/2.39.1/2.39.1.cr
@@ -1,86 +1,10 @@
 class Target < ISM::Software
 
     def prepare
-
-        if option("32Bits") || option("x32Bits")
-            @buildDirectory = true
-        end
-
-        if option("32Bits")
-            @buildDirectoryNames["32Bits"] = "mainBuild-32"
-        end
-
-        if option("x32Bits")
-            @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
-        end
-
         super
 
         if option("Pass1")
             makeDirectory("#{buildDirectoryPath}/var/lib/hwclock")
-        end
-    end
-
-    def configure32Bits
-        if option("Pass1")
-            configureSource(arguments:      "ADJTIME_PATH=/var/lib/hwclock/adjtime                  \
-                                            --libdir=/usr/lib32                                     \
-                                            --host=i686-#{Ism.settings.systemTargetName}-linux-gnu  \
-                                            --docdir=/usr/share/doc/util-linux-2.39.1               \
-                                            --disable-chfn-chsh                                     \
-                                            --disable-login                                         \
-                                            --disable-nologin                                       \
-                                            --disable-su                                            \
-                                            --disable-setpriv                                       \
-                                            --disable-runuser                                       \
-                                            --disable-pylibmount                                    \
-                                            --disable-static                                        \
-                                            --without-python                                        \
-                                            --runstatedir=/run",
-                            path:           buildDirectoryPath(entry: "32Bits"),
-                            environment:    {"CC" =>"gcc -m32"})
-        else
-            configureSource(arguments:      "ADJTIME_PATH=/var/lib/hwclock/adjtime                  \
-                                            --libdir=/usr/lib32                                     \
-                                            --host=i686-#{Ism.settings.systemTargetName}-linux-gnu  \
-                                            --docdir=/usr/share/doc/util-linux-2.39.1               \
-                                            --disable-chfn-chsh                                     \
-                                            --disable-login                                         \
-                                            --disable-nologin                                       \
-                                            --disable-su                                            \
-                                            --disable-setpriv                                       \
-                                            --disable-runuser                                       \
-                                            --disable-pylibmount                                    \
-                                            --disable-static                                        \
-                                            --without-python                                        \
-                                            --without-systemd                                       \
-                                            --without-systemdsystemunitdir",
-                            path:           buildDirectoryPath(entry: "32Bits"),
-                            environment:    {"CC" =>"gcc -m32"})
-        end
-    end
-
-    def configurex32Bits
-        if option("Pass1")
-            configureSource(arguments:      "ADJTIME_PATH=/var/lib/hwclock/adjtime      \
-                                            --libdir=/usr/libx32                        \
-                                            --host=#{Ism.settings.systemTarget}x32      \
-                                            --docdir=/usr/share/doc/util-linux-2.39.1   \
-                                            --disable-chfn-chsh                         \
-                                            --disable-login                             \
-                                            --disable-nologin                           \
-                                            --disable-su                                \
-                                            --disable-setpriv                           \
-                                            --disable-runuser                           \
-                                            --disable-pylibmount                        \
-                                            --disable-static                            \
-                                            --without-python                            \
-                                            --without-systemd                           \
-                                            --without-systemdsystemunitdir",
-                            path:           buildDirectoryPath(entry: "x32Bits"),
-                            environment:    {"CC" =>"gcc -mx32"})
-        else
-
         end
     end
 
@@ -122,57 +46,19 @@ class Target < ISM::Software
                                             --without-systemdsystemunitdir",
                             path:           buildDirectoryPath)
         end
-
-        if option("32Bits")
-            configure32Bits
-        end
-
-        if option("x32Bits")
-            configurex32Bits
-        end
     end
 
     def build
         super
 
-        makeSource(path: buildDirectoryPath(entry: "MainBuild"))
-
-        if option("32Bits")
-            makeSource(path: buildDirectoryPath(entry: "32Bits"))
-        end
-
-        if option("x32Bits")
-            makeSource(path: buildDirectoryPath(entry: "x32Bits"))
-        end
+        makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
 
         makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath} install",
-                    path:       buildDirectoryPath(entry: "MainBuild"))
-
-        if option("32Bits")
-            makeDirectory("#{buildDirectoryPath(entry: "32Bits")}/32Bits")
-            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-            makeSource( arguments:  "DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits install",
-                        path:       buildDirectoryPath(entry: "32Bits"))
-
-            copyDirectory(  "#{buildDirectoryPath(entry: "32Bits")}/32Bits/usr/lib32",
-                            "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib32")
-        end
-
-        if option("x32Bits")
-            makeDirectory("#{buildDirectoryPath(entry: "x32Bits")}/x32Bits")
-            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-            makeSource( arguments:  "DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits install",
-                        path:       buildDirectoryPath(entry: "x32Bits"))
-
-            copyDirectory(  "#{buildDirectoryPath(entry: "x32Bits")}/x32Bits/usr/libx32",
-                            "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/libx32")
-        end
+                    path:       buildDirectoryPath)
     end
 
 end
