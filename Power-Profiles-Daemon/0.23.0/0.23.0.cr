6c5052528abc0d1b26c6a03e59271ee269a2d691
Fulgurance <zohran.londais@gmail.com>
2024-11-16 20:28:24 +0000

    Added power-profiles-daemon

---
+++ Power-Profiles-Daemon/0.23.0/0.23.0.cr
@@ -0,0 +1,40 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand(arguments:  "setup                                  \
+                                    --reconfigure                           \
+                                    #{@buildDirectoryNames["MainBuild"]}    \
+                                    --prefix=/usr                           \
+                                    --buildtype=release                     \
+                                    -Dgtk-doc=false                         \
+                                    -Dsystemdsystemunitdir=no",
+                        path:       mainWorkDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+
+        if option("Openrc")
+            prepareOpenrcServiceInstallation(   path:   "#{workDirectoryPath}/Power-Profiles-Daemon-Init.d",
+                                                name:   "power-profiles-daemon")
+        end
+    end
+
+end
