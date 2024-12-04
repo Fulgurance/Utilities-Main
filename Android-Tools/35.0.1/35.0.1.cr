b1df90adcfc5ee10f39570aa75004ef519ef9f17
Fulgurance <zohran.londais@gmail.com>
2024-12-04 13:50:05 +0000

    Updated android tools installer

--- Android-Tools/35.0.1/35.0.1.cr
+++ Android-Tools/35.0.1/35.0.1.cr
@@ -8,21 +8,30 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "..",
-                        path:       buildDirectoryPath)
+        runCmakeCommand(arguments:      "-DCMAKE_INSTALL_PREFIX=/usr            \
+                                        -DCMAKE_BUILD_TYPE=Release              \
+                                        -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON   \
+                                        -Dprotobuf_MODULE_COMPATIBLE=ON         \
+                                        -DANDROID_TOOLS_LIBUSB_ENABLE_UDEV=ON   \
+                                        -DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON   \
+                                        -B #{buildDirectoryPath}                \
+                                        -G Ninja",
+                        path:           mainWorkDirectoryPath)
     end
 
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        runCmakeCommand(arguments:      "--build #{buildDirectoryPath}",
+                        path:           mainWorkDirectoryPath)
     end
 
     def prepareInstallation
         super
 
-        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
-                    path:       buildDirectoryPath)
+        runCmakeCommand(arguments:      "--install #{buildDirectoryPath}",
+                        path:           mainWorkDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
     end
 
 end
