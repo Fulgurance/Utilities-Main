5b7cf70b359359bc1b8d0199ba1a1e3caa567f1b
Fulgurance <zohran.londais@gmail.com>
2025-04-23 16:08:46 +0100

    Patched cross tool chain building process

--- Coreutils/9.5.0/9.5.0.cr
+++ Coreutils/9.5.0/9.5.0.cr
@@ -6,7 +6,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource(arguments:  "--prefix=/usr                          \
                                         --host=#{Ism.settings.chrootTarget}     \
-                                        --build=#{Ism.settings.chrootTarget}    \
+                                        --build=#{Ism.settings.systemTarget}    \
                                         --enable-install-program=hostname       \
                                         --enable-no-install-program=kill,uptime \
                                         gl_cv_macro_MB_CUR_MAX_good=y",
